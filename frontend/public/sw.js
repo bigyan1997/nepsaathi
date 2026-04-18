5115c54a3cd435f15dd4c83cfa5fec57aeab1a33
Bigyan Karki <karkibigyan05@gmail.com>
2026-04-18 19:51:39 +1000

    feat: PWA without plugin - manual service worker and manifest

--- frontend/public/sw.js
+++ frontend/public/sw.js
@@ -1,14 +1,3 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.jsx";
-
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-);
-
 const CACHE_NAME = "nepsaathi-v1";
 const STATIC_ASSETS = ["/", "/index.html"];
 
