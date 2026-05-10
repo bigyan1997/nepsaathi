63ef251112ad864e4ddfe00bf9176b906a4934f4
Bigyan Karki <karkibigyan05@gmail.com>
2026-05-10 20:24:48 +1000

    fix: load Google Sheets credentials from env var for production

--- backend/feedback/sheets.py
+++ backend/feedback/sheets.py
@@ -1,3 +1,4 @@
+import json
 import logging
 import os
 import threading
@@ -11,16 +12,24 @@ def _append_row(feedback):
         import gspread
         from google.oauth2.service_account import Credentials
 
-        creds_path = config('GOOGLE_SHEETS_CREDENTIALS', default='')
         spreadsheet_id = config('GOOGLE_SHEETS_SPREADSHEET_ID', default='')
-        if not creds_path or not spreadsheet_id:
+        if not spreadsheet_id:
             return
 
-        base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-        creds_abs = os.path.join(base_dir, creds_path)
-
         scopes = ['https://www.googleapis.com/auth/spreadsheets']
-        creds = Credentials.from_service_account_file(creds_abs, scopes=scopes)
+
+        creds_json = config('GOOGLE_SHEETS_CREDENTIALS_JSON', default='')
+        if creds_json:
+            info = json.loads(creds_json)
+            creds = Credentials.from_service_account_info(info, scopes=scopes)
+        else:
+            creds_path = config('GOOGLE_SHEETS_CREDENTIALS', default='')
+            if not creds_path:
+                return
+            base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+            creds_abs = os.path.join(base_dir, creds_path)
+            creds = Credentials.from_service_account_file(creds_abs, scopes=scopes)
+
         client = gspread.authorize(creds)
         sheet = client.open_by_key(spreadsheet_id).sheet1
 
