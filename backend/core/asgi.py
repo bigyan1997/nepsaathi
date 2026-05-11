bea1e93d17f85d4b604c7b9be59afed62d6f0bc9
Bigyan Karki <karkibigyan05@gmail.com>
2026-05-11 11:17:03 +1000

    fix: call django.setup() before importing middleware in asgi.py

--- backend/core/asgi.py
+++ backend/core/asgi.py
@@ -1,10 +1,13 @@
 import os
+import django
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
+django.setup()
+
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from messaging.middleware import JWTWebsocketMiddleware
 import messaging.routing
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
